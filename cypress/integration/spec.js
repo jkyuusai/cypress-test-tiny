8ea60ab7d98a447ea7a429e94dcd75b9260a6543
Joshua Skinner <joshua.skinner@conversahealth.com>
2019-12-03 17:34:24 -0800

    Repro case

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,20 @@
 describe('page', () => {
-  it('works', () => {})
-})
+  const username = 'username';
+  const password = 'password';
+  it('can log in and start a chat', () => {
+    cy.visit("/");
+    cy.get('input[id="user_email"]').type(username);
+    cy.get('input[id="user_password"]').type(password);
+    cy.get('input[type="submit"]').click();
+    cy.request('/');
+    cy.visit('http://my.conversa.local/protocols/PzAhIcX1QIbqfPK2sZtKH64lVkk3OeNHJy5WFi6qxaY?warpspeed=true');
+    cy.contains(/choose a color/);
+  });
+
+  it('should get login page, but do not', () => {
+    cy.visit("/");
+    cy.get('input[id="user_email"]').type(username);
+    cy.get('input[id="user_password"]').type(password);
+    cy.get('input[type="submit"]').click();
+  });
+});
